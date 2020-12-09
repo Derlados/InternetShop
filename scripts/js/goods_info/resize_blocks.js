4b7407d496b1aeddb31b79e376bfb9e9589faecf
Illia <derladoshome@gmail.com>
2020-12-09 23:07:40 +0200

    added goods slider

--- scripts/js/goods_info/resize_blocks.js
+++ scripts/js/goods_info/resize_blocks.js
@@ -1,7 +1,12 @@
 function resize_bllock(idElement) {
     let block = document.getElementById(idElement)
-    if (block.style.height != "max-content")
-        block.style.height = "max-content"
+
+    let height = parseInt(block.children[0].offsetHeight)
+
+    console.log(height)
+
+    if (parseInt(block.style.height) != height)
+        block.style.height = height + "px"
     else
         block.style.height = "190px"
 }
--- scripts/js/goods_info/slider_goods.js
+++ scripts/js/goods_info/slider_goods.js
@@ -1,5 +1,9 @@
-const WIDTH_SLIDER_ITEM = 250
+const WIDTH_SLIDER_ITEM = 250 // Длинна одного товара
+let currentMaxitems = 0 // Текущее максимальное количество товаров которые помещаются
+let lastDirection = 1 // TODO
 
+/** Инициализация объектов при загрузке страницы
+ */
 function init() {
     let goodsSlider = document.getElementById("goods_slider")
     let goodsItems = goodsSlider.children
@@ -8,28 +12,51 @@ function init() {
     resize()
 }
 
+/** Изменение слайдера при изменении размера окна
+ */
 function resize() {
+    // TODO
+    if (checkCurMaxItems() === true)
+        scrollGoods(lastDirection)   
+
     let goodsSlider = document.getElementById("goods_slider")
-    goodsSlider.style.width = getMaxItems() + "px"
+    goodsSlider.style.width = currentMaxitems * WIDTH_SLIDER_ITEM + "px"
 }
 
-function scrollGoods() {
+/** Скроллинг слайдера
+ * @param direction - направление скроллинга (-1 вправл, 1 - влево) 
+ */
+function scrollGoods(direction) {
+    lastDirection = direction
     let goodsSlider = document.getElementById("goods_slider")
     let goodsItems = goodsSlider.children
 
-    let sliderWidth = getMaxItems() * WIDTH_SLIDER_ITEM
-    let maxContentWidth = goodsItems.length * WIDTH_SLIDER_ITEM
-    let currentOffset = goodsItems[0].position.left
-    
-    console.log(goodsItems[0].style.left);
-   
-    for (i = 0; i < goodsItems.length; ++i) {
-        goodsItems[i].style.left = "250px"
-    }
+    // Получение значений сдвигов (максимально доступгый, текущий и предположительный следующий)
+    let maxLeftOffset = (goodsItems.length * WIDTH_SLIDER_ITEM) - currentMaxitems * WIDTH_SLIDER_ITEM
+    let currentOffset = parseInt(goodsItems[0].style.left)
+    let nextOffset = currentMaxitems * WIDTH_SLIDER_ITEM * direction + currentOffset
+
+    // Корректировка чтобы не было пустых мест
+    if (direction == -1 && Math.abs(nextOffset) > maxLeftOffset)
+        nextOffset = -maxLeftOffset
+    else if (direction == 1 && nextOffset > 0)
+        nextOffset = 0;
+
+    // Установка сдвигов
+    for (i = 0; i < goodsItems.length; ++i)
+        goodsItems[i].style.left = nextOffset + "px"
 }
 
-function getMaxItems() {
+//TODO
+/** Проверка и установка максимального количества помещающихся в слайдер товаров
+*/
+function checkCurMaxItems() {
     let goodsSliderHolder = document.getElementById("goods_slider_holder")
     let width = goodsSliderHolder.offsetWidth
-    return (Math.floor(width / WIDTH_SLIDER_ITEM) * WIDTH_SLIDER_ITEM);
+    let newMaxitems = Math.floor(width / WIDTH_SLIDER_ITEM);
+    
+    let returnBool = !(newMaxitems === currentMaxitems)
+
+    currentMaxitems = newMaxitems
+    return returnBool;
 }
